f72864edf17c768361c4289cf91d84cb397f37e6
Ubuntu <ubuntu@ip-172-31-10-40.ap-northeast-1.compute.internal>
2021-07-13 13:59:01 +0900

    m

--- page00.js
+++ page00.js
@@ -1,6 +1,6 @@
 function page00s01(){
   // 画面を開く
-  var winObj = window.open("https://main.d2wj7md4yan98g.amplifyapp.com/index01.html", "_blank");
+  var winObj = window.open("https://https://main.d35izeq5gia0sv.amplifyapp.com/index01.html", "_blank");
   winObj.focus();
 
   delete winObj;
